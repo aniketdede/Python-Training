30e0b984774bd93953e0188df81eb99e450d9d5c
aniket dede <aniketdede12@gmail.com>
2026-06-29 10:47:03 +0530

    learning string manipulation using string methods

--- Strings/string3.py
+++ Strings/string3.py
@@ -1,7 +1,14 @@
+# filling space with @
+
 str = "easy"
 
 print("orginal string",str)
-
-# silling space with @
-
 print("centeres string ", str.center(10,"@"))
+
+
+#using endswith 
+
+str ="open softwares"
+
+print(str.endswith("softwares"))
+print(str.endswith("Softwares"))
